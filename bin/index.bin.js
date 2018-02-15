19a96abbcd1ab068e120e395ce38a9b5e9110ae9
bastiaandewaele <bastiaan@adagio.company>
2018-02-15 13:31:51 +0100

    test

--- bin/index.bin.js
+++ bin/index.bin.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// binary file (global): will execute torchwood.io that is locally installed
+
 const fs = require("fs");
 const path = require("path");
 const process = require("process");
